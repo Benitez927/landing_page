b839f15048eaf7180a271e16f3c1caa68226cfd1
luis benitez <benitezluiz927@gmail.com>
2022-02-20 10:20:12 -0500

    se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,11 @@
+/*show the nav in movil*/
+
+addEventListener('DOMContentLoaded',() =>{
+    const btn__menu = document.querySelector('.btn__menu');
+        if(btn__menu){
+            btn__menu.addEventListener('click',()=>{
+                const nav__menu = document.querySelector('.nav__menu');
+                    nav__menu.classList.toggle('show');
+            })
+        }
+})
